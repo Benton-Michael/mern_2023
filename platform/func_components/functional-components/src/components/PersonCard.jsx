3d217415914253085068ee72595d9b045903137e
Benton-Michael <mbenton002@gmail.com>
2023-06-14 17:00:28 -0700

    completed PersonCard Functional Component with Vite + React

---
+++ platform/func_components/functional-components/src/components/PersonCard.jsx
@@ -0,0 +1,28 @@
+// rafce
+import React from 'react'
+
+const PersonCard = (props) => {
+  return (
+    <div>
+        <h1 style={{ color: 'red' }}>Instructor Michael</h1>
+    </div>
+  )
+}
+
+export default PersonCard
+
+
+
+// import React from "react";
+
+// const PersonCard = (props) => {
+//   return (
+//     <div>
+//       <h1 style={{ color: "red" }}>Instructor Michael</h1>
+//       <p>Age: 94</p>
+//       <p>Hair Color: Aquamarine</p>
+//     </div>
+//   );
+// };
+
+// export default PersonCard;
