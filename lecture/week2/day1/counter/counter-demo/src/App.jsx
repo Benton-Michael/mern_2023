445572c8e31ccdecd565fd50fd497d586de808c8
Benton-Michael <mbenton002@gmail.com>
2023-07-05 18:55:33 -0700

    Added Alert component to the counter-demo app

--- lecture/week2/day1/counter/counter-demo/src/App.jsx
+++ lecture/week2/day1/counter/counter-demo/src/App.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Alert from './components/Alert';
 
 function App() {
   // 03 way
-  const alertButton = () => {
-    alert('You clicked me!')
-  }
+  // const alertButton = () => {
+  //   alert('You clicked me!')
+  // }
   //    Getter   Setter
-  const [count, setCount] = useState(0)
+  // Setter is a function
+  const [count, setCount] = useState(10)
 
   return (
     <>
       <div className='App'>
-        <h1>Synthetic Events</h1>
+        {/* <h1>Synthetic Events</h1> */}
         
-        <button onClick={alert('You clicked me')}>Click Me 1</button>
+        {/* <button onClick={alert('You clicked me')}>Click Me 1</button> */}
         {/* Q: Why might it stop alerting after one click? 01 */}
         {/* 03 continued with a pointer to function
         <button onClick={alertButton}>Click Me!</button> */}
         
         {/* So instead it must be an anonymous function. 02 */}
-        <button onClick={() => alert('you clicked me!')}>Click me 2</button>
+        {/* <button onClick={() => alert('you clicked me!')}>Click me 2</button> */}
 
         <h2>Using the useState hook</h2>
+        <Alert />
+
         <h3>The current count is: {count}</h3>
 
       </div>
---
+++ lecture/week2/day1/counter/counter-demo/src/components/Alert.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Alert = (props) => {
+    const alertButton = () => {
+        alert('You clicked me!')
+    }
+  return (
+    <div>
+        <h1>Synthetic Events</h1>
+        <button onClick={alertButton}>Click me!</button>
+    </div>
+  )
+}
+
+export default Alert;
