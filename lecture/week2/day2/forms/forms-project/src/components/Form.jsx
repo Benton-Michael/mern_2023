bae4be20ad61e12a94de7d30448195816ac14fbd
Benton-Michael <mbenton002@gmail.com>
2023-07-08 19:02:30 -0700

    Added w2 d2 Forms in React

---
+++ lecture/week2/day2/forms/forms-project/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Form = (props) => {
+  return (
+    <div>
+        <form>
+            
+        </form>
+    </div>
+  )
+}
+
+export default Form;
