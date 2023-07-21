93c6e931e185161b67634b043a6d3dc73c6447dd
Benton-Michael <mbenton002@gmail.com>
2023-07-20 18:17:30 -0700

    Added promises example two code with NBA data

---
+++ lecture/week3/day2/js-promise/promise-ex-2.js
@@ -0,0 +1,65 @@
+//* Our function takes in one parameter (request)
+//* request will be a string
+//* We're returning a new Promise object with the callback function
+//* We resolve with the array of objects (not just primitive data types)
+
+function getNbaAthletes (request) {
+    return new Promise((resolve, reject) => {
+        if (request === 'NBA') {
+            resolve([
+                {name: 'Kevin Durant', league: 'NBA', position: 'Small Forward', active: true},
+                {name: 'Charles Barkley', league: 'NBA', position: 'Power Forward', active: false},
+                {name: 'Stephen Curry', league: 'NBA', position: 'Point Guard', active: true},
+                {name: 'Shaq', league: 'NBA', position: 'Center', active: false},
+                {name: 'Damian Lillard', league: 'NBA', position: 'Point Guard', active: true},
+            ])
+        } else {
+            // creates another object with a message key and a hint
+            reject({message: 'Sorry, we cannot understand that request, try again', hint: 'Try NBA as your request' })
+        }
+    })
+}
+// data is our array of objects
+// When passing NBA we will get the resolve
+
+// When passing a random string we will get the reject, (swap out NBA for abc)
+
+// these arrow functions can be written in one line
+getNbaAthletes('abc')
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+//! Output with NBA passed to the function:
+// [
+//     {
+//       name: 'Kevin Durant',
+//       league: 'NBA',
+//       position: 'Small Forward',
+//       active: true
+//     },
+//     {
+//       name: 'Charles Barkley',
+//       league: 'NBA',
+//       position: 'Power Forward',
+//       active: false
+//     },
+//     {
+//       name: 'Stephen Curry',
+//       league: 'NBA',
+//       position: 'Point Guard',
+//       active: true
+//     },
+//     { name: 'Shaq', league: 'NBA', position: 'Center', active: false },
+//     {
+//       name: 'Damian Lillard',
+//       league: 'NBA',
+//       position: 'Point Guard',
+//       active: true
+//     }
+//   ]
+
+//! Output with 'abc' passed to the function:
+// {
+//     message: 'Sorry, we cannot understand that request, try again',
+//     hint: 'Try NBA as your request'
+// }
--- lecture/week3/day2/js-promise/promise.js
+++ lecture/week3/day2/js-promise/promise.js
@@ -9,8 +9,22 @@ const takeOutTheTrash = new Promise((resolve, reject) => {
     }
 })
 
+// ! We can write this using a ternary as well:
+// ignore blue highlights
+// const takeOutTheTrash = new Promise((resolve,reject) => {
+//     trashGotTakenOut
+//     ? resolve("Thanks for taking out the trash!")
+//     : reject('Y you didn\'t take out the trash man 🙁!')
+// });
+
+
+
+
+
 // interacting with the above promise
 
+// method chaining, remember this is not a function, we cannot invoke it
+// promises only take in two params
 takeOutTheTrash
     .then(message => console.log(message))
     .catch(err => console.log(err))
